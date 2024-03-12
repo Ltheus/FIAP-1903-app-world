a385c2f39babbccf7570d7f7732307883437a6dc
Matheus Lopes <matheus.lopes@skyone.solutions>
2024-03-12 14:19:51 -0300

    Mock bem porco do login

--- app/src/main/java/br/com/fiap/appworld/MainActivity.kt
+++ app/src/main/java/br/com/fiap/appworld/MainActivity.kt
@@ -3,12 +3,34 @@ package br.com.fiap.appworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.appworld.ui.theme.AppWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,12 +38,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppWorldTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("World")
+                    LoginScreen()
                 }
             }
         }
@@ -29,9 +50,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoginScreen() {
+
+    var emailState by remember { mutableStateOf("") }
+    var passwordState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "LOGIN",
+            fontSize = 32.sp,
+            fontWeight = (FontWeight(700))
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = emailState,
+            label = {
+                Text(text = "Digite seu e-mail")
+            },
+            placeholder = {
+                Text(text = "Ex.: matheus@email.com")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Email, contentDescription = "")
+            },
+            onValueChange = {
+                emailState = it
+            }
+        )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = passwordState,
+            label = {
+                Text(text = "Digite sua senha")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = "")
+            },
+            onValueChange = {
+                passwordState = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+
+        )
+    }
 }
