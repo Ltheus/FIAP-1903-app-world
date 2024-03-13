abf1305f90baf7eba23cba07e8f3c2c887ae8a46
Matheus Lopes <matheus.lopes@skyone.solutions>
2024-03-13 15:17:42 -0300

    Iniciando criação de telas

--- app/src/main/java/br/com/fiap/appworld/MainActivity.kt
+++ app/src/main/java/br/com/fiap/appworld/MainActivity.kt
@@ -3,34 +3,11 @@ package br.com.fiap.appworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import br.com.fiap.appworld.screens.HomeScreen
 import br.com.fiap.appworld.ui.theme.AppWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,68 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun LoginScreen() {
-
-    var emailState by remember { mutableStateOf("") }
-    var passwordState by remember { mutableStateOf("") }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.primary),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "LOGIN",
-            fontSize = 32.sp,
-            fontWeight = (FontWeight(700))
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            value = emailState,
-            label = {
-                Text(text = "Digite seu e-mail")
-            },
-            placeholder = {
-                Text(text = "Ex.: matheus@email.com")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Email, contentDescription = "")
-            },
-            onValueChange = {
-                emailState = it
-            }
-        )
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            value = passwordState,
-            label = {
-                Text(text = "Digite sua senha")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Lock, contentDescription = "")
-            },
-            onValueChange = {
-                passwordState = it
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-
-        )
     }
 }
---
+++ app/src/main/java/br/com/fiap/appworld/screens/HomeScreen.kt
@@ -0,0 +1,19 @@
+package br.com.fiap.appworld.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import br.com.fiap.appworld.components.AppLayout
+
+@Composable
+fun HomeScreen() {
+    AppLayout(){
+
+    }
+
+}
