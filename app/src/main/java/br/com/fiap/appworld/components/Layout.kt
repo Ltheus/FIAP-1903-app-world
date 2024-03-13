923e141050396d63aaa3d40989cb20fcefb2b146
Matheus Lopes <matheus.lopes@skyone.solutions>
2024-03-13 15:14:51 -0300

    Criando componente de layout

---
+++ app/src/main/java/br/com/fiap/appworld/components/Layout.kt
@@ -0,0 +1,52 @@
+package br.com.fiap.appworld.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import br.com.fiap.appworld.R
+
+@Composable
+fun AppLayout(
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6F9FB))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF033A8E))
+                .height(45.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.infoweather),
+                contentDescription = "InfoWeather Logo",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Layout(content = content) { measurable, constraints ->
+            val placeable =
+                measurable.map { it.measure(constraints.copy(minWidth = 0, minHeight = 0)) }
+
+            val height = 0
+            val width = 0
+            layout(constraints.maxWidth, constraints.maxHeight) {
+                placeable.forEach {
+                    it.place(x = width, y = height)
+                }
+            }
+        }
+    }
+}
